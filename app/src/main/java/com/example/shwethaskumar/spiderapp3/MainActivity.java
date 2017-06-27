6f6bb917c8ade64a2dc535ed4694fcb0810a728d
Shwetha Kumar <swethakumar6@gmail.com>
2017-06-27 21:42:59 +0530

    Normal_mode_completed

---
+++ app/src/main/java/com/example/shwethaskumar/spiderapp3/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.shwethaskumar.spiderapp3;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    MediaPlayer mySound;
+    SensorManager mySensorManager;
+    Sensor myProximitySensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mySound=MediaPlayer.create(this,R.raw.music);
+        mySensorManager = (SensorManager)getSystemService(
+                Context.SENSOR_SERVICE);
+        myProximitySensor = mySensorManager.getDefaultSensor(
+                Sensor.TYPE_PROXIMITY);
+        mySensorManager.registerListener(this, myProximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+            if (event.values[0] == 0)
+                mySound.start();
+            else if (mySound.isPlaying()) {
+                if (event.values[0] != 0) {
+                    mySound.pause();
+                }
+            }
+        }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+public void playMusic()
+{
+    mySound.start();
+}
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mySound.release();
+    }
+}
+
+
